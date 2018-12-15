e3c1081159b37f6f2337679d30e5828fad6c0308
William Hilton <wmhilton@gmail.com>
2018-12-15 16:06:16 -0500

    modernize a bit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const fs = require('fs-extra')
+const _fs = require('fs')
 const git = require('isomorphic-git')
 const GitServer = require('node-git-server')
 const path = require('path')
+
+git.plugins.set('fs', _fs)
 
 ;(async () => {
   const reposDir = 'tmp'
@@ -19,36 +22,38 @@ const path = require('path')
   try {
     await fs.remove(reposDir)
 
-    const repo = { fs, dir: path.join(reposDir, repoName + '.git') }
-    const url = `http://localhost:${gitServerPort}/${path.basename(repo.dir)}`
+    const dir = path.join(reposDir, repoName + '.git')
+    const url = `http://localhost:${gitServerPort}/${path.basename(dir)}`
 
-    await git.init(repo)
+    await git.init({ dir })
 
     // NOTE must commit at least two files here to reproduce hang
-    await fs.writeFile(path.join(repo.dir, '.gitignore'), '')
-    await git.add({ ...repo, filepath: '.gitignore' })
-    await fs.writeFile(path.join(repo.dir, 'README.adoc'), '= README')
-    await git.add({ ...repo, filepath: 'README.adoc' })
-    await git.commit({ ...repo, author, message: 'initial commit' })
+    await fs.writeFile(path.join(dir, '.gitignore'), '')
+    await git.add({ dir, filepath: '.gitignore' })
+    await fs.writeFile(path.join(dir, 'README.adoc'), '= README')
+    await git.add({ dir, filepath: 'README.adoc' })
+    await git.commit({ dir, author, message: 'initial commit' })
 
-    await fs.ensureDir(path.join(repo.dir, 'pages'))
-    await fs.writeFile(path.join(repo.dir, 'pages/page-one.adoc'), '= Page One')
-    await git.add({ ...repo, filepath: 'pages/page-one.adoc' })
-    await git.commit({ ...repo, author, message: 'add page' })
+    await fs.ensureDir(path.join(dir, 'pages'))
+    await fs.writeFile(path.join(dir, 'pages/page-one.adoc'), '= Page One')
+    await git.add({ dir, filepath: 'pages/page-one.adoc' })
+    await git.commit({ dir, author, message: 'add page' })
 
     const repoCloneDir = path.join(reposDir, repoName + '-clone.git')
-    const repoClone = { fs, dir: repoCloneDir, gitdir: repoCloneDir }
+    const repoClone = { dir: repoCloneDir, gitdir: repoCloneDir }
     await git.clone({ ...repoClone, url, depth: 1, noCheckout: true, noTags: true })
       // NOTE uncommenting the next line will circumvent the hang
-      //.then(() => fs.emptyDir(path.join(repoClone.gitdir, 'refs', 'heads')))
+    // await fs.emptyDir(path.join(repoClone.gitdir, 'refs', 'heads'))
 
-    await fs.writeFile(path.join(repo.dir, 'pages/page-one.adoc'), '= Page One\n\nContent updated!')
-    await git.add({ ...repo, filepath: 'pages/page-one.adoc' })
-    await git.commit({ ...repo, author, message: 'save changes' })
+    await fs.writeFile(path.join(dir, 'pages/page-one.adoc'), '= Page One\n\nContent updated!')
+    await git.add({ dir, filepath: 'pages/page-one.adoc' })
+    await git.commit({ dir, author, message: 'save changes' })
 
     await git.fetch({ ...repoClone, depth: 1, noCheckout: true })
-    await git.resolveRef({ ...repoClone, ref: 'remotes/origin/master' })
-      .then((oid) => git.readObject({ ...repoClone, oid }))
+    const oid = await git.resolveRef({ ...repoClone, ref: 'remotes/origin/master' })
+    console.log('prepare to hang', oid)
+    await git.readObject({ ...repoClone, oid })
+    console.log('didnt hang')
     console.log('done')
   } finally {
     await new Promise((resolve, reject) => gitServer.server.close((err) => (err ? reject(err) : resolve())))
